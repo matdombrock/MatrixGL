67b1067224bf194a8115127e48bcb96aa903f777
Mathieu Dombrock <matdombrock@gmail.com>
2021-04-04 05:32:12 -0700

    Update examples

--- examples/example.ino
+++ examples/example.ino
@@ -4,79 +4,10 @@
 #define MAX_DEVICES 4
 #define LENGTHX 32
 #define LENGTHY 8
-#define INTENSITY 0
 
-MatrixGL matrix(CS_PIN,MAX_DEVICES, LENGTHX, LENGTHY, INTENSITY);
+MatrixGL matrix(CS_PIN,MAX_DEVICES);
 void setup() {
   // put your setup code here, to run once:
-}
-
-void demoLine(){
-  for(int i = 0; i < LENGTHX; i++){
-    matrix.delayF();
-    matrix.clear();
-    matrix.drawLine(0,0,i,i/4);
-  }
-  for(int i = 0; i < 8; i++){
-    matrix.delayF();
-    matrix.clear();
-    matrix.drawLine(0,0,LENGTHX,7-i);
-  }
-  for(int i = 0; i < 8; i++){
-    matrix.delayF();
-    matrix.clear();
-    matrix.drawLine(0,0,LENGTHX,i);
-  }
-  for(int i = 0; i < LENGTHX; i++){
-    matrix.delayF();
-    matrix.clear();
-    matrix.drawLine(0,0,LENGTHX-1-i,7-i/4);
-  }
-  
-  for(int i = 0; i < 8; i++){
-    matrix.delayF();
-    matrix.clear();
-    matrix.drawLine(0,0,8,i);
-  }
-  for(int i = 0; i < 8; i++){
-    matrix.delayF();
-    matrix.clear();
-    matrix.drawLine(0,0,8,7-i);
-  }
-
-  for(int i = 0; i < LENGTHX/2; i++){
-    matrix.delayF();
-    matrix.clear();
-    matrix.drawLine(0,0,LENGTHX/2,i);
-  }
-  for(int i = 0; i < LENGTHX/2; i++){
-    matrix.delayF();
-    matrix.clear();
-    matrix.drawLine(0,0,LENGTHX/2,7-i);
-  }
-
-  for(int i = 0; i < 24; i++){
-    matrix.delayF();
-    matrix.clear();
-    matrix.drawLine(0,0,24,i);
-  }
-  for(int i = 0; i < 24; i++){
-    matrix.delayF();
-    matrix.clear();
-    matrix.drawLine(0,0,24,7-i);
-  }
-
-  for(int i = 0; i < 16; i++){
-    matrix.delayF();
-    matrix.clear();
-    matrix.drawLine(i,i/2,i*2,1/4);
-  }
-
-  for(int i = 0; i < 16; i++){
-    matrix.delayF();
-    matrix.clear();
-    matrix.drawLine(i,i/4,i*2,i/2);
-  }
 }
 
 void demoLowLevel(){
@@ -96,20 +27,140 @@ void demoLowLevel(){
   matrix.delayF();
 }
 
-void demoDrawPath(){
-  int points[] = {0,0, 4,4, 7,6, 32,0};
-  matrix.lock();
+void demoPathRings(){
+  int X = matrix.lenX() - 1;
+  int Y = matrix.lenY() - 1;
+  int path[] = {0,0, X,0, X,Y, 0,Y, 0,0};
+  matrix.drawPath(path, 10);
+  matrix.delayF();
+  X--;
+  Y--;
+  int path2[] = {1,1, X,1, X,Y, 1,Y, 1,1};
+  matrix.drawPath(path2, 10);
+  matrix.delayF();
+  X--;
+  Y--;
+  int path3[] = {2,2, X,2, X,Y, 2,Y, 2,2};
+  matrix.drawPath(path3, 10);
+  matrix.delayF();
+  X--;
+  Y--;
+  int path4[] = {3,3, X,3, X,Y, 3,Y, 3,3};
+  matrix.drawPath(path4, 10);
+  matrix.delayF();
+  matrix.delayF();
   matrix.clear();
-  matrix.drawPath(points, 8);
+  //
+  //
+  matrix.lock();
+  matrix.drawPath(path3, 10);
+  matrix.drawPath(path2, 10);
+  matrix.drawPath(path, 10);
   matrix.unlock();
+  matrix.delayF();
+  matrix.clear();
+  matrix.lock();
+  matrix.drawPath(path2, 10);
+  matrix.drawPath(path, 10);
+  matrix.unlock();
+  matrix.delayF();
+  matrix.clear();
+  matrix.drawPath(path, 10);
+  matrix.delayF();
+  matrix.delayF();
+  matrix.clear();
+  //
+  matrix.drawPath(path, 10);
+  matrix.delayF();
+  matrix.clear();
+  matrix.drawPath(path2, 10);
+  matrix.delayF();
+  matrix.clear();
+  matrix.drawPath(path3, 10);
+  matrix.delayF();
+  matrix.clear();
+  matrix.drawPath(path4, 10);
+  matrix.delayF();
+  matrix.delayF();
+  matrix.clear();
+  matrix.drawPath(path3, 10);
+  matrix.delayF();
+  matrix.clear();
+  matrix.drawPath(path2, 10);
+  matrix.delayF();
+  matrix.clear();
+  matrix.drawPath(path, 10);
+  matrix.delayF();
+  matrix.clear();
 }
 
+void demoLinesFill(){
+  int X = matrix.lenX() - 1;
+  int Y = matrix.lenY() - 1;
+  for(int i = 0; i<=Y;i++){
+    matrix.drawLine(0,0, X,i);
+    matrix.delayF();
+  }
+  for(int i = 0; i<8;i++){
+    matrix.drawLine(X,Y, 0,Y-i);
+    matrix.delayF();
+  }
+  matrix.clear();
+}
+void demoIntensity(){
+  int y = 0;
+  int lenX = matrix.lenX()-1;
+  for(int i = 0; i<=15;i++){
+    matrix.drawLine(0,y, lenX,y);
+    y++;
+    if(y>7){
+      y=0;
+    }
+    matrix.setIntensity(i);
+    matrix.delayN(5);
+    matrix.clear();
+  }
+}
 
+void demoRandomLines(){
+  int X = matrix.lenX();
+  int Y = matrix.lenY();
+  matrix.drawLine(random(X), random(Y), random(X), random(Y));
+  matrix.delayF();
+  matrix.clear();
+}
+void demoRandomLines2(){
+  int X = matrix.lenX();
+  int Y = matrix.lenY();
+  for(int i = 0; i<16; i++){
+    matrix.drawLine(random(X), random(Y), random(X), random(Y));
+    matrix.delayF();
+  }
+  matrix.clear();
+}
+
+void demoSnow(){
+  int X = matrix.lenX();
+  int Y = matrix.lenY();
+  for(int i = 0; i<256; i++){
+    matrix.mx->setPoint(random(Y), random(X), true);
+    matrix.delayF();
+  }
+  matrix.clear();
+}
+
+void demoRandomNoise(){
+  int X = matrix.lenX();
+  int Y = matrix.lenY();
+  matrix.lock();
+  matrix.clear();
+  for(int i = 0; i<(X*Y); i++){
+    matrix.drawPoint(random(X), random(Y)); 
+  }
+  matrix.unlock();
+  matrix.delayF();
+}
 void loop() {
-  // put your main code here, to run repeatedly:
-  //demoLowLevel();
-  
-  demoLine();
-  //demoDrawPath();
-  //delay(1000);
+  matrix.drawPoint(0,0);
+  //demoRandomNoise();
 }
